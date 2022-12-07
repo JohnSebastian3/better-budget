b99aea4695be88eceab9c88a584aea1c3d6bd057
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-07 18:06:02 -0500

    Add Register page and include registration form that hits /register endpoint in server

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const register = () => {
+    axios.post('http://localhost:4000/register', {
+      email,
+      password
+    }, {
+      withCredentials: true
+    }).then(res => {
+      if(res.data === 'OK') {
+        window.location.href = '/login';
+      }
+    })
+  }
+
+  return( <div>
+    <h1>Register</h1>
+    <input type="text" placeholder="Email" onChange={handleEmailChange}/>
+    <input type="text" placeholder="Password" onChange={handlePasswordChange}/>
+    <button onClick={register}>Register</button>
+  </div>
+  );
 }
