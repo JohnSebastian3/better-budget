364dc78d962a43091a4404da46115043570f3e2c
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-07 18:00:50 -0500

    Add databse user interface to match data received from mongodb

--- server/src/interfaces/UserInterface.ts
+++ server/src/interfaces/UserInterface.ts
@@ -1,5 +1,11 @@
 export interface UserInterface {
-  email: string,
-  username: string,
-  password: string,
+  email: string;
+  username: string;
+}
+
+export interface DatabaseUserInterface {
+  email: string;
+  username: string;
+  password: string;
+  _id: string;
 }
