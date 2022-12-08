a8417fcf36f128bdc19002318db2adece75356bc
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-08 16:31:13 -0500

    Add imports

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,9 @@
 import React  from 'react'
+import Header from './Header/Header';
+import classes from './Homepage.module.css';
 
 export default function Homepage() {
   return (
-    <main className='main'>
-      <h1 className='main__h1'>It's time to make money work in your favor.</h1>
-      <h2 className='main__h2'>Gain freedom by taking charge of your finances. </h2>
-    </main>
+    <Header></Header>
   )
 }
