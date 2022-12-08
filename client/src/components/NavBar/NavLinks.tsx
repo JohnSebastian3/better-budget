b2f2af3a48f9c2c83635d19432d66b255e4eb5c3
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-08 12:52:18 -0500

    Add additional conditional rendering to position elements depending on whether user is on mobile or not

--- client/src/components/NavBar/NavLinks.tsx
+++ client/src/components/NavBar/NavLinks.tsx
@@ -10,7 +10,7 @@ const NavLinks: React.FC<{ isMobile: boolean; closeMobileMenu: () => void }> = (
   const ctx = useContext(userContext);
   const animateFrom = { opacity: 0, y: -40 };
   const animateTo = { opacity: 1, y: 0 };
-  
+
   const logout = () => {
     axios
       .get("http://localhost:4000/logout", { withCredentials: true })
@@ -22,62 +22,83 @@ const NavLinks: React.FC<{ isMobile: boolean; closeMobileMenu: () => void }> = (
   };
   return (
     <>
-      <motion.ul
-        initial={animateFrom}
-        animate={animateTo}
-        transition={{ delay: 0.1 }}
-        className={classes["nav-links"]}
-      >
-        <motion.li
-          initial={animateFrom}
-          animate={animateTo}
-          transition={{ delay: 0.2 }}
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-        >
-          <Link to={"/"}>Home</Link>
-        </motion.li>
-        {ctx ? (
-          <>
+      {props.isMobile ? (
+        <>
+          <motion.ul
+            initial={animateFrom}
+            animate={animateTo}
+            transition={{ delay: 0.1 }}
+            className={classes["nav-links"]}
+          >
             <motion.li
               initial={animateFrom}
               animate={animateTo}
-              transition={{ delay: 0.3 }}
+              transition={{ delay: 0.2 }}
               onClick={() => props.isMobile && props.closeMobileMenu()}
             >
-              <Link to={"/dashboard"}>Dashboard</Link>
+              <Link to={"/"}>Home</Link>
             </motion.li>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.4 }}
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
+            {ctx ? (
+              <>
+                <motion.li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.3 }}
+                  onClick={() => props.isMobile && props.closeMobileMenu()}
+                >
+                  <Link to={"/dashboard"}>Dashboard</Link>
+                </motion.li>
+                <motion.li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.4 }}
+                  onClick={() => props.isMobile && props.closeMobileMenu()}
+                >
+                  <Link onClick={logout} to={"/login"}>
+                    Logout
+                  </Link>
+                </motion.li>
+              </>
+            ) : (
+              <>
+                <motion.li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.3 }}
+                  onClick={() => props.isMobile && props.closeMobileMenu()}
+                >
+                  <Link to={"/login"}>Login</Link>
+                </motion.li>
+                <motion.li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.4 }}
+                  onClick={() => props.isMobile && props.closeMobileMenu()}
+                >
+                  <Link to={"/register"}>Register</Link>
+                </motion.li>
+              </>
+            )}
+          </motion.ul>
+        </>
+      ) : (
+        <ul className={classes["nav-links"]}>
+          <Link to={"/"}>Home</Link>
+          {ctx ? (
+            <>
+              <Link to={"/dashboard"}>Dashboard</Link>
               <Link onClick={logout} to={"/login"}>
                 Logout
               </Link>
-            </motion.li>
-          </>
-        ) : (
-          <>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.3 }}
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
-              <Link to={"/login"}>Login</Link>
-            </motion.li>
-            <motion.li
-              initial={animateFrom}
-              animate={animateTo}
-              transition={{ delay: 0.4 }}
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-            >
+            </>
+          ) : (
+            <>
               <Link to={"/register"}>Register</Link>
-            </motion.li>
-          </>
-        )}
-      </motion.ul>
+              <Link to={"/login"}>Login</Link>
+            </>
+          )}
+        </ul>
+      )}
     </>
   );
 };
