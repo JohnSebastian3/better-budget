cef13bef1d96841a8884598a4807ac9f8787dc44
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-10 14:58:00 -0500

    Add button component to return a button that uses props for every attribute

--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import style from "./Button.module.css";
 
 type ButtonProps = {
-  type: 'submit' | 'reset' | 'button';
+  type: "submit" | "reset" | "button";
   value: string;
-  classes: string;
-}
+  kind: string;
+};
 
 const Button = (props: ButtonProps) => {
   return (
-    <button type={props.type} className={props.classes}>{props.value}</button>
-  )
-}
+    <button type={props.type} className={`${style.btn} ${style[props.kind]}`}>
+      {props.value}
+    </button>
+  );
+};
 
 export default Button;
