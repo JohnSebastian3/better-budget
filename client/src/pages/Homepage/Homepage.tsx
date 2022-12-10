520e6288980283018bc078b0f2fd73747eef752c
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-10 14:55:51 -0500

    Add react fragment and main component

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,12 @@
-import React  from 'react'
-import Header from './Header/Header';
-import classes from './Homepage.module.css';
+import React from "react";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
 
 export default function Homepage() {
   return (
-    <Header></Header>
-  )
+    <React.Fragment>
+      <Header></Header>
+      <Main></Main>
+    </React.Fragment>
+  );
 }
