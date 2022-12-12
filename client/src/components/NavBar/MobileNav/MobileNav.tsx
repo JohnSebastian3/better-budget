760161ca4b903f1926fe992c605237e9e333d5d9
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-12 15:24:39 -0500

    Import AnimatePresence to allow exit motion for the navlinks to work

--- client/src/components/NavBar/MobileNav/MobileNav.tsx
+++ client/src/components/NavBar/MobileNav/MobileNav.tsx
@@ -3,24 +3,27 @@ import NavLinks from "../NavLinks/NavLinks";
 import classes from "./MobileNav.module.css";
 import { AiOutlineMenu } from "react-icons/ai";
 import { SlClose } from "react-icons/sl";
-const MobileNav: React.FC = props => {
+import { AnimatePresence } from "framer-motion";
+
+const MobileNav: React.FC = (props) => {
   const [open, setOpen] = useState<boolean>(false);
 
   const handleHamburgerClick = () => {
-    if(open) {
-      document.body.style.overflow = "visible"
-    } else if(!open) {
-      document.body.style.overflow = "hidden"
+    if (open) {
+      document.body.style.overflow = "visible";
+    } else if (!open) {
+      document.body.style.overflow = "hidden";
     }
 
     setOpen(!open);
-  }
+  };
 
   const hamburgerIcon = (
     <AiOutlineMenu
       className={classes.hamburger}
       size="25px"
       color="#3D9970"
+      style={{ cursor: "pointer" }}
       onClick={handleHamburgerClick}
     />
   );
@@ -29,18 +32,21 @@ const MobileNav: React.FC = props => {
       className={classes.hamburger}
       size="25px"
       color="#3D9970"
+      style={{ cursor: "pointer" }}
       onClick={handleHamburgerClick}
     />
   );
   const closeMobileMenu = () => {
     setOpen(false);
-  }
+  };
   return (
     <nav className={classes["mobile-navigation"]}>
       {open ? closeHamburgerIcon : hamburgerIcon}
-      {open && <NavLinks isMobile={true} closeMobileMenu={closeMobileMenu}/>}
+      <AnimatePresence>
+        {open && <NavLinks isMobile={true} closeMobileMenu={closeMobileMenu} />}
+      </AnimatePresence>
     </nav>
   );
-}
+};
 
 export default MobileNav;
