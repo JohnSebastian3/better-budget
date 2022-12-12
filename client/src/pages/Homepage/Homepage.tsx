31d33048f76e897f3677ffe5e95414b2413b709f
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-12 11:45:05 -0500

    Add component to homepage

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 
 export default function Homepage() {
   return (
     <React.Fragment>
-      <Header></Header>
-      <Main></Main>
+      <Header />
+      <Main />
+      <Footer />
     </React.Fragment>
   );
 }
