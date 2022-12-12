0ecca78d947ffaae61f174703bcf4b0eb5a4f56a
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-12 11:42:04 -0500

    Remove unused getUser function

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,55 +1,59 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import Footer from "../../components/Footer/Footer";
 export default function Login() {
-
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const login = () => {
-    axios.post("http://localhost:4000/login", {
-      email,
-      password
-    }, {
-      withCredentials: true
-    })
-      .then(res => {
-        if(res.data === 'OK') {
-          window.location.href = "/dashboard"
+    axios
+      .post(
+        "http://localhost:4000/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        if (res.data === "OK") {
+          window.location.href = "/dashboard";
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
-
-  const getUser = () => {
-    axios.get('http://localhost:4000/user', {
-      withCredentials: true,
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }, ).then(res => {
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
+      });
+  };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
-  }
+  };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
-  }
+  };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input type="text" placeholder='email' onChange={handleEmailChange} value={email}/>
-      <input type="text" placeholder='password' onChange={handlePasswordChange} value={password}/>
-      <button onClick={login}>Login</button>
-    </div>
-  )
+    <>
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="email"
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <button onClick={login}>Login</button>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
