9c8da731410c54e6c3777c51be5cbb9c38d54e5e
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-12 11:40:39 -0500

    Add main intro component

---
+++ client/src/pages/Homepage/Main/MainIntro/MainIntro.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { GiReceiveMoney } from "react-icons/gi";
+import style from "./MainIntro.module.css";
+const MainIntro = () => {
+  return (
+    <section className={style["main__intro"]}>
+      <div className="container">
+        <div className={style['main__intro-content']}>
+          <GiReceiveMoney
+            size="80px"
+            style={{ marginBottom: "20px" }}
+          ></GiReceiveMoney>
+          <h2>Money matters, and so do you.</h2>
+          <p>
+            BetterBudger is a tool to help you get a headstart on your finances
+            and build the tomorrow that you want.
+          </p>
+          <p>
+            Rid yourself of the gloomy days worrying about debt and bills so you
+            can have a happier, healthier and wealthier life.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainIntro;
