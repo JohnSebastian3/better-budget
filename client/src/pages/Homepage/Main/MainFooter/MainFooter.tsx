fc825ab8c533172962f84c02c10adf9cb8a13a18
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-12 11:40:58 -0500

    Add main footer component

---
+++ client/src/pages/Homepage/Main/MainFooter/MainFooter.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "./MainFooter.module.css";
+import { AiOutlineDashboard } from "react-icons/ai";
+import Button from "../../../../components/UI/Button/Button";
+import { Link } from "react-router-dom";
+const MainFooter = () => {
+  return (
+    <section className={style["main__footer"]}>
+      <div className="container">
+        <div className={style["main__footer-content"]}>
+          <AiOutlineDashboard
+            size="80px"
+            style={{ marginBottom: "20px" }}
+          ></AiOutlineDashboard>
+          <h2>Stop feeling guilty when you spend.</h2>
+          <p>Start spending with condifence and without guilt.</p>
+          <Link to={"/register"}>
+            <Button
+              type={"button"}
+              value={"Create Your Budget"}
+              kind={"btn-primary-green"}
+            ></Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainFooter;
