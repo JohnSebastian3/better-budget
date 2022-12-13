b16cf8be7dcbb1485558e83a1eb3081a0080f7c0
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-13 12:27:10 -0500

    Add months array and render Dashboard expense form and expenses components

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,30 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../../context/UserContext'
+import React, { useContext } from "react";
+import { userContext } from "../../context/UserContext";
+import DashboardExpenseForm from "./DashboardExpenseForm/DashboardExpenseForm";
+import DashboardExpenses from "./DashboardExpenses/DashboardExpenses";
 
 export default function Dashboard() {
-  const ctx = useContext(UserContext);
+  const ctx = useContext(userContext);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "Octover",
+    "November",
+    "December",
+  ];
   return (
     <div>
-      <h1>Currently Logged in user: {ctx.username}</h1>  
+      <h1>{months[new Date().getMonth()]}</h1>
+      <h2>{new Date().getFullYear()}</h2>
+      <DashboardExpenseForm />
+      <DashboardExpenses />
     </div>
-  )
+  );
 }
