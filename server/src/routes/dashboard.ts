cdf93e6bc58ad024e2a9780a0f7b91c3667385ca
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-13 12:24:24 -0500

    Change name of route

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -3,6 +3,6 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboard");
 
 router.get("/", dashboardController.getDashboard);
-router.post('/expense', dashboardController.createExpense);
+router.post('/addExpense', dashboardController.createExpense);
 
 module.exports = router;
