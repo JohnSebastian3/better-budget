d539590880d80a169a1054456b77ab2448874b1a
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-13 12:29:52 -0500

    Modify getDashboard and createExpense functions to return appropriate data

--- server/src/controllers/dashboard.ts
+++ server/src/controllers/dashboard.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import Expense from '../models/Expense';
 module.exports = {
-  getDashboard: async (req: Request, res: Response) => {
-    res.send("OK");
+  getDashboard: async (req: any, res: any) => {
+    const expenses = await Expense.find({id: req.user._id});
+    console.log('expenses for user:', expenses);
+    res.send(expenses);
   },
-  createExpense: async (req: Request, res: Response) => {
+  createExpense: async (req: any, res: any) => {
     const newExpense = new Expense({
       title: req.body.title,
-      value: 100,
+      value: Number(req.body.value),
+      category: req.body.category,
+      date: Date.now(),
+      user: req.user.id
     })
     const createdExpense = await newExpense.save();
     res.json(createdExpense);
