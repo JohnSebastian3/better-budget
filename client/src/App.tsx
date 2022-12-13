d267e8ea8832c388644215b0af2635f9e5522487
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-13 12:26:12 -0500

    Correct context name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,11 @@ import Homepage from "./pages/Homepage/Homepage";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import { UserContext } from "./context/UserContext";
+import { userContext } from "./context/UserContext";
 import { useContext } from "react";
 import './App.css'
 function App() {
-  const ctx = useContext(UserContext);
+  const ctx = useContext(userContext);
   return (
     <BrowserRouter>
         <NavBar />
