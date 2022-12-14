de6111fa5bbfc9e3796878013ff0c96be412f9b1
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-14 12:29:50 -0500

    Add types to props

--- client/src/pages/Dashboard/DashboardExpenses/DashboardExpenses.tsx
+++ client/src/pages/Dashboard/DashboardExpenses/DashboardExpenses.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ExpensesContext from "../../../context/ExpensesContext";
+import { ExpenseInterface } from "../../../Interfaces/ExpenseInterface";
 import DashboardExpensesList from "./DashboardExpensesList/DashboardExpensesList";
-const DashboardExpenses = () => {
+const DashboardExpenses = (props: {
+  expenses: ExpenseInterface[];
+  selectedMonth: number;
+  selectedYear: number;
+}) => {
   return (
     <ExpensesContext>
-      <DashboardExpensesList />
+      <DashboardExpensesList expenses={props.expenses} selectedMonth={props.selectedMonth} selectedYear={props.selectedYear} />
     </ExpensesContext>
   );
 };
