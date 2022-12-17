696c2110d304c6e810814d8b09b2e4bccd315a1f
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-17 17:12:29 -0500

    Move navbar component to everywhere except dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,28 +7,28 @@ import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { userContext } from "./context/UserContext";
 import { useContext } from "react";
-import './App.css'
+import "./App.css";
 function App() {
   const ctx = useContext(userContext);
   return (
     <BrowserRouter>
-        <NavBar />
-          <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-            {ctx ? (
-              <>
-              <Route path="/dashboard" element={<Dashboard />}></Route>
-              <Route path="/login" element={<Dashboard />}></Route> 
-              <Route path="/register" element={<Dashboard />}></Route>
-              </>
-            ) : (
-              <>
-                <Route path="/register" element={<Register />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/dashboard" element={<Login />}></Route>
-              </>
-            )}
-          </Routes>
+      
+      <Routes>
+        <Route path="/" element={<><NavBar /><Homepage /></>}></Route>
+        {ctx ? (
+          <>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/login" element={<Dashboard />}></Route>
+            <Route path="/register" element={<Dashboard />}></Route>
+          </>
+        ) : (
+          <>
+            <Route path="/register" element={<><NavBar /><Register /></>}></Route>
+            <Route path="/login" element={<><NavBar /><Login /></>}></Route>
+            <Route path="/dashboard" element={<><NavBar /><Login /></>}></Route>
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
   );
 }
