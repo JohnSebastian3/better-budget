85c46f1284b982cc817d7cd04276844fb3aad176
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-17 17:15:16 -0500

    Modify graph

--- client/src/pages/Dashboard/DashboardGraph/DashboardGraph.tsx
+++ client/src/pages/Dashboard/DashboardGraph/DashboardGraph.tsx
@@ -1,49 +1,58 @@
 import React from "react";
 import style from "./DashboardGraph.module.css";
-import { Chart, ArcElement, Tooltip} from "chart.js";
+import { Chart, ArcElement, Tooltip } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 Chart.register(ArcElement, Tooltip);
 
-const config = {
-  type: 'Doughnut',
-  data: {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [300, 50, 100],
-        backgroundColor: [
-          "#40b480",
-          "#73e7b3",
-          "rgb(255, 205, 86)",
-        ],
-        hoverOffset: 5,
-        borderRadius: 6,
-        spacing: 2,
-      },
-    ],
-  },
-  options: {
-    maintainAspectRatio: false,
-    responsive: true,
-    // radius: 100,
-    cutout: 100,
-  },
-};
-const DashboardGraph = () => {
+const DashboardGraph = (props: {
+  totalExpenses: number;
+  totalIncome: number;
+}) => {
+
+  const moneyLeft = (props.totalIncome - props.totalExpenses < 0) ? 0 : props.totalIncome - props.totalExpenses;
+  const config = {
+    type: "Doughnut",
+    data: {
+      labels: [
+        "Amount Spent",
+        "Amount Left",
+      ],
+      datasets: [
+        {
+          data: [
+            props.totalExpenses,
+            props.totalExpenses > 0 ? moneyLeft : 1, 
+          ],
+          backgroundColor: [
+            "#ff6961",
+            "#40b480",
+          ],
+          hoverOffset: 5,
+          borderRadius: 6,
+          spacing: 1,
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      // radius: 100,
+      cutout: 100,
+    },
+  };
+
   return (
     <div className={style["dashboard__graph"]}>
       <div className={style["dashboard__item"]}>
         <div className={style["dashboard__chart"]}>
           <Doughnut {...config}></Doughnut>
-          <div className={style['dashboard__info']}>
+          <div className={style["dashboard__info"]}>
             <h3 className={style["dashboard__heading"]}>Total</h3>
-            <span className={style["dashboard__total-value"]}>${0}</span>
+            <span className={style["dashboard__total-value"]}>
+              ${props.totalExpenses} spent, ${moneyLeft}{" "}
+              left
+            </span>
           </div>
         </div>
       </div>
