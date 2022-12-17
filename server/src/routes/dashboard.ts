d35f7247b7e6c41e6697bebc77c97c9d481707fe
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-17 17:13:14 -0500

    Rename route

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -3,6 +3,6 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboard");
 
 router.get("/", dashboardController.getDashboard);
-router.post('/addExpense', dashboardController.createExpense);
+router.post('/addTransaction', dashboardController.createTransaction);
 
 module.exports = router;
