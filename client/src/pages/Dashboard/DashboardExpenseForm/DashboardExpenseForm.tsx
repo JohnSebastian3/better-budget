162b98749240c99d7ffd7ffd75fbaa83ae211091
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-17 17:18:13 -0500

    Add conditional check to form to display different options based on whether transaction is expense or income

--- client/src/pages/Dashboard/DashboardExpenseForm/DashboardExpenseForm.tsx
+++ client/src/pages/Dashboard/DashboardExpenseForm/DashboardExpenseForm.tsx
@@ -1,27 +1,30 @@
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import React, { useForm } from "react-hook-form";
 import Button from "../../../components/UI/Button/Button";
-import { ExpenseInterface } from "../../../Interfaces/ExpenseInterface";
+import { TransactionInterface } from "../../../Interfaces/TransactionInterface";
 
 const DashboardExpenseForm = (props: {
-  onAddExpense: (expense: ExpenseInterface) => void;
+  onAddExpense: (expense: TransactionInterface) => void;
   onChangeDay: (event: ChangeEvent<HTMLInputElement>) => void;
   selectedMonth: number;
   selectedYear: number;
   selectedDay: number;
 }) => {
-
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [isIncome, setIsIncome] = useState<boolean>(true);
   const { register, handleSubmit, reset } = useForm();
+
+
   const onSubmit = (data: any) => {
     axios
       .post(
-        "http://localhost:4000/dashboard/addExpense",
+        "http://localhost:4000/dashboard/addTransaction",
         {
-          title: data.title,
-          category: data.category,
+          title: data.title || "Income",
+          category: data.category || "Job",
           value: Number(data.value),
+          isIncome: isIncome,
           date: new Date(data.date),
         },
         {
@@ -36,8 +39,8 @@ const DashboardExpenseForm = (props: {
       });
 
     reset();
+    setIsIncome(true);
   };
-
 
   const onChangeDate = (event: ChangeEvent<HTMLInputElement>) => {
     const pickedDate = event.target.value.split("-");
@@ -48,6 +51,11 @@ const DashboardExpenseForm = (props: {
     setSelectedDate(date);
   };
 
+  const setIncome = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    value === "Income" ? setIsIncome(true) : setIsIncome(false);
+  };
+
   return (
     <form
       action="dashboard/addExpense"
@@ -55,31 +63,100 @@ const DashboardExpenseForm = (props: {
       onSubmit={handleSubmit(onSubmit)}
     >
       <input
-        type="text"
-        placeholder="what did  you spend it on"
-        {...register("title")}
-      ></input>
-      <label htmlFor="dateOfExpense">
-        Enter Expense Date
-        <input
-          type="date"
-          {...register("date")}
-          id="dateOfExpense"
-          value={`${selectedDate.toLocaleDateString().split("/")[2]}-${
-            selectedDate.toLocaleDateString().split("/")[0].padStart(2, '0')
-          }-${selectedDate.toLocaleDateString().split("/")[1].padStart(2, '0')}`}
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            onChangeDate(event);
-          }}
-        />
-      </label>
-      <input type="text" placeholder="category?" {...register("category")} />
-      <input type="number" placeholder="amount spent?" {...register("value")} />
-      <Button
-        type="submit"
-        value="Add Expense"
-        kind="btn--primary--green"
-      ></Button>
+        type="radio"
+        id="income"
+        name="income/expense"
+        value="Income"
+        checked={isIncome}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setIncome(event)}
+      />
+      <label htmlFor="income">Income</label>
+      <input
+        type="radio"
+        id="expense"
+        name="income/expense"
+        value="Expense"
+        checked={!isIncome}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setIncome(event)}
+      />
+      <label htmlFor="expense">Expense</label>
+      {isIncome ? (
+        <>
+          <label htmlFor="dateOfExpense">
+            Enter Income Date
+            <input
+              type="date"
+              {...register("date")}
+              id="dateOfIncome"
+              value={`${
+                selectedDate.toLocaleDateString().split("/")[2]
+              }-${selectedDate
+                .toLocaleDateString()
+                .split("/")[0]
+                .padStart(2, "0")}-${selectedDate
+                .toLocaleDateString()
+                .split("/")[1]
+                .padStart(2, "0")}`}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                onChangeDate(event);
+              }}
+            />
+          </label>
+          <input
+            type="number"
+            placeholder="amount earned?"
+            {...register("value")}
+          />
+          <Button
+            type="submit"
+            value="Add Income"
+            kind="btn--primary--green"
+          ></Button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Where you spend"
+            {...register("title")}
+          ></input>
+          <label htmlFor="dateOfExpense">
+            Enter Expense Date
+            <input
+              type="date"
+              {...register("date")}
+              id="dateOfExpense"
+              value={`${
+                selectedDate.toLocaleDateString().split("/")[2]
+              }-${selectedDate
+                .toLocaleDateString()
+                .split("/")[0]
+                .padStart(2, "0")}-${selectedDate
+                .toLocaleDateString()
+                .split("/")[1]
+                .padStart(2, "0")}`}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                onChangeDate(event);
+              }}
+            />
+          </label>
+          <input
+            type="text"
+            placeholder="category?"
+            {...register("category")}
+          />
+          <input
+            type="number"
+            placeholder="amount spent?"
+            {...register("value")}
+          />
+          <Button
+            type="submit"
+            value="Add Expense"
+            kind="btn--primary--green"
+          ></Button>
+        </>
+      )}
     </form>
   );
 };
