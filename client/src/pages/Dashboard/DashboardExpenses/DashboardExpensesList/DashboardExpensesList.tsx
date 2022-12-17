95c9f05b1789c395e23ade31544c0230c1d7f127
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-17 17:16:45 -0500

    Adjust props names to match correctly

--- client/src/pages/Dashboard/DashboardExpenses/DashboardExpensesList/DashboardExpensesList.tsx
+++ client/src/pages/Dashboard/DashboardExpenses/DashboardExpensesList/DashboardExpensesList.tsx
@@ -1,32 +1,19 @@
 import React from "react";
-import { ExpenseInterface } from "../../../../Interfaces/ExpenseInterface";
+import { TransactionInterface } from "../../../../Interfaces/TransactionInterface";
 // import { expensesContext } from "../../../../context/ExpensesContext";
 
 const DashboardExpensesList = (props: {
-  expenses: ExpenseInterface[];
-  selectedMonth: number;
-  selectedYear: number;
+  expenses: TransactionInterface[];
+  totalExpenses: number;
+  totalIncome: number;
 }) => {
   // const ctx = useContext(expensesContext);
 
-  const filteredExpenses = props.expenses.filter((expense: ExpenseInterface) => {
-    const expenseMonth = new Date(expense.date).getUTCMonth();
-    const expenseYear = new Date(expense.date).getUTCFullYear();
-    // console.log(`expense ${expense.title} was bought on ${expenseMonth}, ${expenseYear}`);
-    return expenseMonth === props.selectedMonth && expenseYear === props.selectedYear;
-  })
-
-  const calculateFullExpenses = (expenses: ExpenseInterface[]) => {
-    return expenses.reduce((acc, curr) => {
-      return acc += curr.value;
-    }, 0)
-  }
-
   return (
     <>
-    <h1>Total spent this month: {calculateFullExpenses(filteredExpenses)}</h1>
+    <h1>Total spent this month ${props.totalExpenses} and you made ${props.totalIncome}</h1>
     <ul>
-      {filteredExpenses.map((expense, index) => {
+      {props.expenses.map((expense, index) => {
         return <li key={index}>{expense.title} for {expense.value}</li>;
       })}
     </ul>
