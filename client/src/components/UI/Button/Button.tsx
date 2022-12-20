d722c8fe5fd58428edb03bde49159af1482c46aa
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-20 12:58:14 -0500

    Add onclick prop to allow button to have an onclick listener

--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
   type: "submit" | "reset" | "button";
   value: string;
   kind: string;
+  onClick?: () => void;
 };
 
 const Button = (props: ButtonProps) => {
   return (
-    <button type={props.type} className={`${style.btn} ${style[props.kind]}`}>
+    <button type={props.type} className={`${style.btn} ${style[props.kind]}`} onClick={props.onClick ? props.onClick : () => {}}>
       {props.value}
     </button>
   );
