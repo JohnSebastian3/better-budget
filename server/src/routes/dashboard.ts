d74514e232b0f8400c3a932c657f223f4760df0b
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-23 18:06:47 -0500

    Rename routes

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -3,6 +3,9 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboard");
 
 router.get("/", dashboardController.getDashboard);
-router.post('/addTransaction', dashboardController.createTransaction);
+router.get('/subcategories/:category', dashboardController.getSubcategories);
+router.post('/addTransaction', dashboardController.addTransaction);
+router.post('/addCategory', dashboardController.addCategory);
+router.post('/addSubcategory/:category', dashboardController.addSubcategory)
 
 module.exports = router;
