16b9557a60fc2938dc6f9ddd07ad812412465b77
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-23 18:08:38 -0500

    Instantiate new user with default categories and subcategories

--- server/src/controllers/home.ts
+++ server/src/controllers/home.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import bcrypt from "bcryptjs";
 import { DatabaseUserInterface } from "../interfaces/UserInterface";
+import mongoose from "mongoose";
+import Category from "../models/Category";
 module.exports = {
-  registerUser: async (req: Request, res: Response) => {
+  registerUser: async (req: any, res: any) => {
     const { email, username, password } = req?.body;
     if (
       !email ||
@@ -16,36 +18,87 @@ module.exports = {
       res.send("Improper values");
     }
 
-     User.findOne({ email }, async (err: Error, doc: DatabaseUserInterface) => {
+    User.findOne({ email }, async (err: Error, doc: DatabaseUserInterface) => {
       if (err) throw err;
       if (doc) res.send("Email already registered");
       if (!doc) {
-        User.findOne({username}, async (err: Error, doc: DatabaseUserInterface) => {
-          if(err) throw err;
-          if(doc) res.send("Username is in use");
-          if(!doc) {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            const newUser = new User({
-              username,
-              email,
-              password: hashedPassword,
-            });
-            await newUser.save();
-            res.sendStatus(200);
+        User.findOne(
+          { username },
+          async (err: Error, doc: DatabaseUserInterface) => {
+            if (err) throw err;
+            if (doc) res.send("Username is in use");
+            if (!doc) {
+              const hashedPassword = await bcrypt.hash(req.body.password, 10);
+              const newUser = new User({
+                username,
+                email,
+                password: hashedPassword
+              });
+              const createdUser = await newUser.save();
+
+              const categoryIncome = new Category({
+                title: 'Income',
+                subcategories: ['Paychecks'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categoryIncome.save();
+
+              const categorySpending = new Category({
+                title: 'Spending',
+                subcategories: ['Groceries'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categorySpending.save();
+
+              const categoryBills = new Category({
+                title: 'Bills',
+                subcategories: ['Rent'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categoryBills.save();
+
+              const categorySubscriptions = new Category({
+                title: 'Subscriptions',
+                subcategories: ['Streaming'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categorySubscriptions.save();
+
+              const categoryDebt = new Category({
+                title: 'Debt',
+                subcategories: ['Credit Card'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categoryDebt.save();
+
+              const categoryGifts = new Category({
+                title: 'Gifts',
+                subcategories: ['Charity'],
+                user: new mongoose.Types.ObjectId(createdUser._id)
+              })
+          
+              await categoryGifts.save();
+
+              res.sendStatus(200);
+            }
           }
-        })
-       
-       
+        );
       }
     });
+    
   },
   getUser: (req: any, res: Response) => {
     res.send(req.user);
   },
   logoutUser: (req: any, res: any) => {
     req.logout((err: Error) => {
-      if(err) return err.message;
+      if (err) return err.message;
       res.sendStatus(200);
     });
-  }
+  },
 };
