93f6e9e968395454f356d53d29cc95f308ead03c
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-23 18:07:36 -0500

    Add subcategory property and make user a mongoose ID object

--- server/src/models/Transaction.ts
+++ server/src/models/Transaction.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-// const ObjectId = mongoose.Types.ObjectId;
 
 const TransactionSchema = new Schema({
   title: String,
   category: String,
+  subcategory: String,
   value: Number,
   isIncome: Boolean,
   date: Date,
-  user: String
+  user: mongoose.Types.ObjectId,
 })
 
 const TransactionModel = mongoose.model('Transaction', TransactionSchema);
