8a1307bf29a256c3a09708b64a14d202a74985d4
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-24 15:05:57 -0500

    Add username input

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -2,36 +2,64 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Register() {
-
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
-  }
+  };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
-  }
+  };
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
 
   const register = () => {
-    axios.post('http://localhost:4000/register', {
-      email,
-      password
-    }, {
-      withCredentials: true
-    }).then(res => {
-      if(res.data === 'OK') {
-        window.location.href = '/login';
-      }
-    })
-  }
+    axios
+      .post(
+        "http://localhost:4000/register",
+        {
+          email,
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        if (res.data === "OK") {
+          window.location.href = "/login";
+        }
+      });
+  };
 
-  return( <div>
-    <h1>Register</h1>
-    <input type="text" placeholder="Email" onChange={handleEmailChange}/>
-    <input type="password" placeholder="Password" onChange={handlePasswordChange}/>
-    <button onClick={register}>Register</button>
-  </div>
+  return (
+    <div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={handleEmailChange}
+        value={email}
+      />
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={handleUsernameChange}
+        value={username}
+      ></input>
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={handlePasswordChange}
+        value={password}
+      />
+      <button onClick={register}>Register</button>
+    </div>
   );
 }
