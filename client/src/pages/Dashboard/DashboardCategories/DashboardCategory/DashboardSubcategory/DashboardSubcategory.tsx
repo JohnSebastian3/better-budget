e852769cd01fef74685a8240ce48c5d3cf46ecfe
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-24 18:12:36 -0500

    Add dashboard subcategory component

--- client/src/pages/Dashboard/DashboardCategories/DashboardCategory/DashboardSubcategory/DashboardSubcategory.tsx
+++ client/src/pages/Dashboard/DashboardCategories/DashboardCategory/DashboardSubcategory/DashboardSubcategory.tsx
@@ -1,29 +1,51 @@
-import React from 'react'
-import { TransactionInterface } from '../../../../../Interfaces/TransactionInterface';
-
+import axios from "axios";
+import React, { useState } from "react";
+import { TransactionInterface } from "../../../../../Interfaces/TransactionInterface";
+import style from "./DashboardSubcategory.module.css";
 const DashboardSubcategory = (props: {
   subcategory: string;
+  category: string;
   transactions: TransactionInterface[];
 }) => {
-
-  let transactions = props.transactions.filter(transaction => {
+  let transactions = props.transactions.filter((transaction) => {
     return transaction.subcategory === props.subcategory;
-  })
+  });
+
+  const [deleteIsVisible, setDeleteIsVisible] = useState<boolean>(false);
+
+  const showDeleteOption = (event: any) => {
+    setDeleteIsVisible(true);
+  };
+
+  const hideDeleteOption = (event: any) => {
+    setDeleteIsVisible(false);
+  };
+
+  const deleteSubcategory = () => {
+    axios.delete(`http://localhost:4000/dashboard/deleteSubcategory/${props.category}/${props.subcategory}`)
+  }
 
   return (
-    <div>
-    <h3>{props.subcategory}</h3>
-    <ul>
-    {transactions?.map((transaction, index) => {
-      return (
-        <li key={index}>
-          {transaction.title} for {transaction.value}
-        </li>
-      );
-    })}
-  </ul>
+    <div className={style["dashboard__subcategory"]}>
+      <div
+        className={style.test}
+        onMouseOver={(event) => showDeleteOption(event)}
+        onMouseLeave={(event) => hideDeleteOption(event)}
+      >
+        <h3>{props.subcategory}</h3>
+        {deleteIsVisible ? <a onClick={deleteSubcategory}>Delete</a> : ""}
+      </div>
+      <ul>
+        {transactions?.map((transaction, index) => {
+          return (
+            <li key={index}>
+              {transaction.title} for {transaction.value}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardSubcategory
+export default DashboardSubcategory;
