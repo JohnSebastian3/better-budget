d3af5d43456854b023f132362fcceff9a474d877
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-26 18:08:48 -0500

    Remove unneeded route

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -3,9 +3,8 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboard");
 
 router.get("/", dashboardController.getDashboard);
-router.get('/subcategories/:category', dashboardController.getSubcategories);
 router.post('/addTransaction', dashboardController.addTransaction);
 router.post('/addCategory', dashboardController.addCategory);
-router.post('/addSubcategory/:category', dashboardController.addSubcategory)
-
+router.post('/addSubcategory/:category', dashboardController.addSubcategory);
+router.delete('/deleteSubcategory/:category/:subcategory', dashboardController.deleteSubcategory);
 module.exports = router;
