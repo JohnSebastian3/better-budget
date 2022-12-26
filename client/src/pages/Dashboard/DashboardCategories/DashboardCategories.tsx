c193ffd1c83afd46aabd0aac4058cd871abe63a7
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-26 18:20:51 -0500

    Add onSubmit function to form and add conditional rendering to subcategory form

--- client/src/pages/Dashboard/DashboardCategories/DashboardCategories.tsx
+++ client/src/pages/Dashboard/DashboardCategories/DashboardCategories.tsx
@@ -1,78 +1,95 @@
-import React, { useState } from "react";
-import ExpensesContext from "../../../context/ExpensesContext";
+import React, { useEffect, useState } from "react";
 import { TransactionInterface } from "../../../Interfaces/TransactionInterface";
+import { CategoryInterface } from "../../../Interfaces/CategoryInterface";
 import style from "./DashboardCategories.module.css";
 import Card from "../../../components/UI/Card/Card";
 import Button from "../../../components/UI/Button/Button";
 import DashboardCategory from "./DashboardCategory/DashboardCategory";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 const DashboardCategories = (props: {
   transactions: TransactionInterface[];
-  totalExpenses: number;
-  totalIncome: number;
+  categories: CategoryInterface[];
+  addCategory: (category: CategoryInterface) => void;
+  onAddSubcategory: (subcategory: string, category: string) => void;
 }) => {
-  const [categories, setCategories] = useState<string[]>([
-    "Income",
-    "Spending",
-    "Bills",
-    "Subscriptions",
-    "Debt",
-    "Charity",
-  ]);
-  
   const { register, handleSubmit, reset } = useForm();
   const [isFormShown, setisFormShown] = useState<boolean>(false);
 
   const showForm = () => {
     setisFormShown(true);
-  }
+  };
 
   const hideForm = () => {
     setisFormShown(false);
-  }
+  };
 
   const onSubmit = (data: any) => {
-    setCategories((prev) => {
-      return [...prev, data.category]
-    })
+    let categoryTitle = data.category.toLowerCase();
+    categoryTitle = categoryTitle[0].toUpperCase() + categoryTitle.slice(1);
+    let newCategory = { title: categoryTitle, subcategories: [] };
+    axios
+      .post(
+        "http://localhost:4000/dashboard/addCategory",
+        {
+          newCategory,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        props.addCategory(newCategory);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     reset();
     setisFormShown(false);
+  };
+
+  const onAddSubcategory = (subcategory: string, category: string) => {
+    props.onAddSubcategory(subcategory, category);
   }
 
   return (
-    <ExpensesContext>
-      <div className={style["dashboard__categories"]}>
-        {categories.map((category, index) => {
-          return (
-            <Card key={index}>
-              <DashboardCategory
-                transactions={props.transactions}
-                category={category}
-              />
-            </Card>
-          );
-        })}
+    <div className={style["dashboard__categories"]}>
+      {props.categories.map((category, index) => {
+        return (
+          <Card key={index}>
+            <DashboardCategory
+              transactions={props.transactions}
+              category={category.title}
+              subcategories={category.subcategories}
+              onAddSubcategory={onAddSubcategory}
+            />
+          </Card>
+        );
+      })}
 
-        {isFormShown ? (
-           <form className={style['dashboard__categories__form']} onSubmit={handleSubmit(onSubmit)}>
-           <input type="text" placeholder="Category" {...register('category')}/>
-           <button type="submit">Add</button>
-           <button type="button" onClick={hideForm}>Cancel</button>
-         </form>
-        ) : (
-          <Button
+      {isFormShown ? (
+        <form
+          className={style["dashboard__categories__form"]}
+          method="POST"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input type="text" placeholder="Category" {...register("category")} />
+          <button type="submit">Add</button>
+          <button type="button" onClick={hideForm}>
+            Cancel
+          </button>
+        </form>
+      ) : (
+        <Button
           type="submit"
           value="Add Category"
           kind="btn--primary--green"
+          disabled={false}
           onClick={showForm}
         ></Button>
-        )}
-
-       
-
-      
-      </div>
-    </ExpensesContext>
+      )}
+    </div>
   );
 };
 
