2d5ef638aaa25bd2e0079cea87e94a30a52baa1f
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-12-29 22:27:18 -0500

    Add delete category function

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -6,5 +6,7 @@ router.get("/", dashboardController.getDashboard);
 router.post('/addTransaction', dashboardController.addTransaction);
 router.post('/addCategory', dashboardController.addCategory);
 router.post('/addSubcategory/:category', dashboardController.addSubcategory);
+router.put('/setSubcategoryBudget/:category/:subcategory', dashboardController.setSubcategoryBudget);
 router.delete('/deleteSubcategory/:category/:subcategory', dashboardController.deleteSubcategory);
+router.delete('/deleteCategory/:category', dashboardController.deleteCategory);
 module.exports = router;
