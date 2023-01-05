ff173634443ffe7cd17ea891d90358f9fc118859
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-04 21:43:28 -0500

    Add Dashboard graph and transaction list components to stats

--- client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardStats.tsx
+++ client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardStats.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TransactionInterface } from '../../../../Interfaces/TransactionInterface';
 import DashboardGraph from '../../DashboardGraph/DashboardGraph'
 import style from './DashboardStats.module.css';
-const DashboardStats = (props: {totalExpenses: number; totalIncome: number}) => {
+import DashboardTransactionList from './DashboardTransactionList/DashboardTransactionList';
+const DashboardStats = (props: {totalExpenses: number; totalIncome: number, transactions: TransactionInterface[]}) => {
+
+  const [currentlySelected, setCurrentlySelected] = useState<string>('Graph');
+
   return (
     <div>
-      <DashboardGraph totalExpenses={props.totalExpenses} totalIncome={props.totalIncome}/>
-    </div>
+      {currentlySelected === 'Graph'? (
+              <DashboardGraph totalExpenses={props.totalExpenses} totalIncome={props.totalIncome}/>
+
+      ) : (
+          <DashboardTransactionList transactions={props.transactions}></DashboardTransactionList>
+      )}
+      <button onClick={() => setCurrentlySelected('Graph')}>Graph</button>
+      <button onClick={() => setCurrentlySelected('Transactions')}>Transactions</button>
+    </div> 
   )
 }
 
