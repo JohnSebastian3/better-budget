4370f5ac96b87a970b1c4388ee20e10e7fabbef5
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-05 21:03:13 -0500

    Add function to update categories state with new budget to cause re-render

--- client/src/pages/Dashboard/DashboardBudget/DashboardBudget.tsx
+++ client/src/pages/Dashboard/DashboardBudget/DashboardBudget.tsx
@@ -9,7 +9,7 @@ import DashboardTransactionForm from "../DashboardTransactionForm/DashboardTrans
 import DashboardCategories from "../DashboardCategories/DashboardCategories";
 import { CategoryInterface } from "../../../Interfaces/CategoryInterface";
 
-const DashboardBudget = () => {
+const DashboardBudget = (props: {month: number, year: number}) => {
   const ctx = useContext(userContext);
 
   const [transactions, setTransactions] = useState<TransactionInterface[]>([]);
@@ -17,7 +17,21 @@ const DashboardBudget = () => {
   const [month, setMonth] = useState<number>(new Date().getMonth());
   const [year, setYear] = useState<number>(new Date().getFullYear());
   const [categories, setCategories] = useState<CategoryInterface[]>([]);
-  
+  // const [filteredCategories, sertFilteredCategories] = useState<CategoryInterface[]>([]);
+
+  console.log('month index:', month);
+  console.log('year index:', year);
+  const filteredCategories = categories.filter(category => {
+    if(categories) {
+      console.log(`${category.title} has date with month: ${new Date(category.date).getUTCMonth()}`)
+      return new Date(category.date).getUTCMonth() === month && new Date(category.date).getUTCFullYear() === year;
+    }
+    return category;
+  })
+
+  console.log('fdfdfdf:',filteredCategories);
+
+
   useEffect(() => {
     axios
       .get("http://localhost:4000/dashboard", { withCredentials: true })
@@ -26,7 +40,8 @@ const DashboardBudget = () => {
           (category: CategoryInterface) => {
             return {
               title: category.title,
-              subcategories: [...category.subcategories]
+              subcategories: [...category.subcategories],
+              date: category.date,
             };
           }
         );
@@ -63,19 +78,19 @@ const DashboardBudget = () => {
     });
   };
 
-  const addSubcategory = (subcategory: string, category: string) => {
+  const addSubcategory = (subcategory: string, category: string, date: Date) => {
     setCategories((prev) => {
       for(const cat of prev) {
         if(cat.title === category) {
           console.log(`Adding subcat ${subcategory.toUpperCase()} to ${category.toUpperCase()}`)
-          cat.subcategories.push({title: subcategory, budget: 0});
+          cat.subcategories.push({title: subcategory, budget: 0, date: date});
         }
       }
       return prev
     });
   }
 
-  const deleteCategory = (category: string | undefined) => {
+  const deleteCategory = (category: string) => {
     console.log('running');
     setCategories((prev) => {
       return prev.filter(cat => cat.title !== category);
@@ -107,6 +122,28 @@ const DashboardBudget = () => {
     }
   }, 0);
 
+  const updateBudget = (subcategory: {
+    title: string;
+    budget: number;
+  }, category: string) => {
+    setCategories((prev) => {
+      const updated = prev.map(cat => {
+        if(cat.title === category) {
+          cat.subcategories = cat.subcategories.map(subcat => {
+            if(subcat.title === subcategory.title) {
+              subcat.budget = subcategory.budget;
+              return subcat;
+            } 
+            return subcat;
+          })
+          return cat;
+        } 
+        return cat;
+      })
+      return updated;
+    })
+  }
+
   return (
     <div className={style["dashboard__budget"]}>
       <div className={style["dashboard__content"]}>
@@ -118,10 +155,11 @@ const DashboardBudget = () => {
         />
         <DashboardCategories
           transactions={filteredTransactions}
-          categories={categories}
+          categories={filteredCategories}
           addCategory={addCategory}
           onAddSubcategory={addSubcategory}
           onDeleteCategory={deleteCategory}
+          onUpdateBudget={updateBudget}
         />
       </div>
       <div className={style["dashboard__stats"]}>
