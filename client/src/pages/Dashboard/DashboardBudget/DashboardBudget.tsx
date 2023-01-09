9a94c89078f95d5935417a15d24f9404bbdf44d2
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-09 12:58:13 -0500

    Fix code to work with the updating of the category and transaction interfaces

--- client/src/pages/Dashboard/DashboardBudget/DashboardBudget.tsx
+++ client/src/pages/Dashboard/DashboardBudget/DashboardBudget.tsx
@@ -9,7 +9,7 @@ import DashboardTransactionForm from "../DashboardTransactionForm/DashboardTrans
 import DashboardCategories from "../DashboardCategories/DashboardCategories";
 import { CategoryInterface } from "../../../Interfaces/CategoryInterface";
 
-const DashboardBudget = (props: {month: number, year: number}) => {
+const DashboardBudget = (props: { month: number; year: number }) => {
   const ctx = useContext(userContext);
 
   const [transactions, setTransactions] = useState<TransactionInterface[]>([]);
@@ -17,20 +17,18 @@ const DashboardBudget = (props: {month: number, year: number}) => {
   const [month, setMonth] = useState<number>(new Date().getMonth());
   const [year, setYear] = useState<number>(new Date().getFullYear());
   const [categories, setCategories] = useState<CategoryInterface[]>([]);
-  // const [filteredCategories, sertFilteredCategories] = useState<CategoryInterface[]>([]);
 
-  console.log('month index:', month);
-  console.log('year index:', year);
-  const filteredCategories = categories.filter(category => {
-    if(categories) {
-      console.log(`${category.title} has date with month: ${new Date(category.date).getUTCMonth()}`)
-      return new Date(category.date).getUTCMonth() === month && new Date(category.date).getUTCFullYear() === year;
+  console.log("OUR CATEGORIES:", categories);
+  console.log("OUR TRANSACTIONS:", transactions);
+
+  const filteredCategories = categories.filter((category) => {
+    if (categories) {
+      return category.dateMonth === month && category.dateYear === year;
     }
     return category;
-  })
+  });
 
-  console.log('fdfdfdf:',filteredCategories);
-
+  console.log("filtered categories:", filteredCategories);
 
   useEffect(() => {
     axios
@@ -41,7 +39,8 @@ const DashboardBudget = (props: {month: number, year: number}) => {
             return {
               title: category.title,
               subcategories: [...category.subcategories],
-              date: category.date,
+              dateMonth: category.dateMonth,
+              dateYear: category.dateYear,
             };
           }
         );
@@ -78,33 +77,55 @@ const DashboardBudget = (props: {month: number, year: number}) => {
     });
   };
 
-  const addSubcategory = (subcategory: string, category: string, date: Date) => {
+  const addSubcategory = (
+    subcategory: string,
+    category: string,
+    dateMonth: number,
+    dateYear: number
+  ) => {
     setCategories((prev) => {
-      for(const cat of prev) {
-        if(cat.title === category) {
-          console.log(`Adding subcat ${subcategory.toUpperCase()} to ${category.toUpperCase()}`)
-          cat.subcategories.push({title: subcategory, budget: 0, date: date});
+      for (const cat of prev) {
+        if (
+          cat.title === category &&
+          cat.dateMonth === dateMonth &&
+          cat.dateYear === dateYear
+        ) {
+          console.log(
+            `Adding subcat ${subcategory.toUpperCase()} to ${category.toUpperCase()}`
+          );
+          cat.subcategories.push({
+            title: subcategory,
+            budget: 0,
+            dateMonth,
+            dateYear,
+          });
         }
       }
-      return prev
+      return prev;
     });
-  }
+  };
 
-  const deleteCategory = (category: string) => {
-    console.log('running');
+  const deleteCategory = (category: string, dateMonth: number, dateYear: number) => {
     setCategories((prev) => {
-      return prev.filter(cat => cat.title !== category);
-    })
-  }
-
+      return prev.filter((cat) => {
+        if(cat.dateMonth === dateMonth && cat.dateYear === dateYear) {
+          return cat.title !== category;
+        } else {
+          return true;
+        }
+      });
+    });
+  };
 
   const filteredTransactions = transactions.filter(
     (transaction: TransactionInterface) => {
-      const transactionMonth = new Date(transaction.date).getUTCMonth();
-      const transactionYear = new Date(transaction.date).getUTCFullYear();
+      const transactionMonth = transaction.dateMonth;
+      const transactionYear = transaction.dateYear;
       return transactionMonth === month && transactionYear === year;
     }
   );
+
+  console.log("OUR filtered transactions:", filteredTransactions);
 
   const totalExpenses = filteredTransactions.reduce((acc, curr) => {
     if (!curr.isIncome) {
@@ -122,27 +143,68 @@ const DashboardBudget = (props: {month: number, year: number}) => {
     }
   }, 0);
 
-  const updateBudget = (subcategory: {
-    title: string;
-    budget: number;
-  }, category: string) => {
+  const updateBudget = (
+    subcategory: {
+      title: string;
+      budget: number;
+      dateMonth: number;
+      dateYear: number;
+    },
+    category: string,
+    budgetAmount: number
+  ) => {
     setCategories((prev) => {
-      const updated = prev.map(cat => {
-        if(cat.title === category) {
-          cat.subcategories = cat.subcategories.map(subcat => {
-            if(subcat.title === subcategory.title) {
-              subcat.budget = subcategory.budget;
+      const updated = prev.map((cat) => {
+        if (
+          cat.title === category &&
+          cat.dateMonth === subcategory.dateMonth &&
+          cat.dateYear === subcategory.dateYear
+        ) {
+          cat.subcategories = cat.subcategories.map((subcat) => {
+            if (subcat.title === subcategory.title) {
+              subcat.budget = budgetAmount;
               return subcat;
-            } 
+            }
             return subcat;
-          })
+          });
           return cat;
-        } 
+        }
         return cat;
-      })
+      });
       return updated;
-    })
-  }
+    });
+  };
+
+  const createBudget = () => {
+    axios
+      .post(
+        "http://localhost:4000/dashboard/createBudget",
+        { month, year },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        setCategories((prev) => {
+          return [...prev, ...res.data];
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteTransactions = (transactionsToDelete: TransactionInterface[]) => {
+    const filteredTransactions = transactions.filter(
+      (trxToRemove) =>
+        !transactionsToDelete.find(
+          (trx) =>
+            trx.title === trxToRemove.title &&
+            trx.dateDay === trxToRemove.dateDay &&
+            trx.dateMonth === trxToRemove.dateMonth &&
+            trx.dateYear === trxToRemove.dateYear
+        )
+    );
+    setTransactions(filteredTransactions)
+  };
 
   return (
     <div className={style["dashboard__budget"]}>
@@ -156,10 +218,15 @@ const DashboardBudget = (props: {month: number, year: number}) => {
         <DashboardCategories
           transactions={filteredTransactions}
           categories={filteredCategories}
+          month={month}
+          year={year}
+          day={day}
           addCategory={addCategory}
           onAddSubcategory={addSubcategory}
           onDeleteCategory={deleteCategory}
           onUpdateBudget={updateBudget}
+          onCreateBudget={createBudget}
+          deleteTransactions={deleteTransactions}
         />
       </div>
       <div className={style["dashboard__stats"]}>
