4246134d54e5bb3b0612153a671371dd0a1dcffe
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-11 16:25:02 -0500

    Add conditional rendering

--- client/src/pages/Dashboard/DashboardCategories/DashboardCategories.tsx
+++ client/src/pages/Dashboard/DashboardCategories/DashboardCategories.tsx
@@ -10,12 +10,35 @@ import axios from "axios";
 const DashboardCategories = (props: {
   transactions: TransactionInterface[];
   categories: CategoryInterface[];
+  totalSetBudget: number;
+  totalSetIncome: number;
+  isOverBudget: boolean;
   year: number;
   month: number;
   day: number;
   addCategory: (category: CategoryInterface) => void;
-  onAddSubcategory: (subcategory: string, category: string, dateMonth: number, dateYear: number) => void;
-  onDeleteCategory: (category: string, dateMonth: number, dateYear: number) => void;
+  onAddSubcategory: (
+    subcategory: string,
+    category: string,
+    dateMonth: number,
+    dateYear: number
+  ) => void;
+  onDeleteCategory: (
+    category: string,
+    dateMonth: number,
+    dateYear: number
+  ) => void;
+  onDeleteSubcategory: (
+    subcategories: {
+      title: string;
+      budget: number;
+      dateMonth: number;
+      dateYear: number;
+    }[],
+    category: string,
+    dateMonth: number,
+    dateYear: number
+  ) => void;
   onUpdateBudget: (
     subcategory: {
       title: string;
@@ -24,7 +47,7 @@ const DashboardCategories = (props: {
       dateYear: number;
     },
     category: string,
-    budgetAmount: number,
+    budgetAmount: number
   ) => void;
   onCreateBudget: () => void;
   deleteTransactions: (transactionsToDelete: TransactionInterface[]) => void;
@@ -43,8 +66,21 @@ const DashboardCategories = (props: {
   const onSubmit = (data: any) => {
     let categoryTitle = data.category.toLowerCase();
     categoryTitle = categoryTitle[0].toUpperCase() + categoryTitle.slice(1);
-    let newCategory = { title: String(categoryTitle), subcategories: [], dateMonth: props.month, dateYear: props.year };
-    axios
+    let newCategory = {
+      title: String(categoryTitle),
+      subcategories: [],
+      dateMonth: props.month,
+      dateYear: props.year,
+    };
+
+    let valid = true;
+    for(const cat of props.categories) {
+      if(cat.title === categoryTitle) {
+        valid = false;
+      }
+    }
+    if(valid) {
+      axios
       .post(
         "http://localhost:4000/dashboard/addCategory",
         {
@@ -60,16 +96,23 @@ const DashboardCategories = (props: {
       .catch((err) => {
         console.log(err);
       });
-
+    } else {
+      //PLaceholder for now
+      alert('Category already exists');
+    }
+    
     reset();
     setisFormShown(false);
   };
 
   const deleteCategory = (category: string) => {
     axios
-      .delete(`http://localhost:4000/dashboard/deleteCategory/${category}/${props.month}/${props.year}/${props.day}`, {
-        withCredentials: true,
-      })
+      .delete(
+        `http://localhost:4000/dashboard/deleteCategory/${category}/${props.month}/${props.year}/${props.day}`,
+        {
+          withCredentials: true,
+        }
+      )
       .then((res) => {
         props.onDeleteCategory(category, props.month, props.year);
       })
@@ -78,22 +121,31 @@ const DashboardCategories = (props: {
       });
   };
 
-  const onAddSubcategory = (subcategory: string, category: string, dateMonth: number, dateYear: number) => {
+  const onAddSubcategory = (
+    subcategory: string,
+    category: string,
+    dateMonth: number,
+    dateYear: number
+  ) => {
     props.onAddSubcategory(subcategory, category, dateMonth, dateYear);
   };
 
   const onUpdateBudget = (
-    subcategory: { title: string; budget: number, dateMonth: number, dateYear: number },
+    subcategory: {
+      title: string;
+      budget: number;
+      dateMonth: number;
+      dateYear: number;
+    },
     category: string,
-    budgetAmount: number,
+    budgetAmount: number
   ) => {
     props.onUpdateBudget(subcategory, category, budgetAmount);
   };
 
   const deleteTransactions = (transactionsToDelete: TransactionInterface[]) => {
     props.deleteTransactions(transactionsToDelete);
-  }
-
+  };
 
   return (
     <div className={style["dashboard__categories"]}>
@@ -101,46 +153,62 @@ const DashboardCategories = (props: {
         <button onClick={props.onCreateBudget}>Start a budget here</button>
       ) : (
         <>
-        {props.categories.map((category, index) => {
-          return (
-            <Card key={index}>
-              <DashboardCategory
-                transactions={props.transactions}
-                category={category}
-                subcategories={category.subcategories}
-                year={props.year}
-                month={props.month}
-                day={props.day}
-                onDeleteCategory={deleteCategory}
-                onAddSubcategory={onAddSubcategory}
-                onUpdateBudget={onUpdateBudget}
-                deleteTransactions={deleteTransactions}
-              />
-            </Card>
-          );
-        })}
-        {isFormShown ? (
-          <form
-            className={style["dashboard__categories__form"]}
-            method="POST"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <input type="text" placeholder="Category" {...register("category")} />
-            <button type="submit">Add</button>
-            <button type="button" onClick={hideForm}>
-              Cancel
-            </button>
-          </form>
+        {props.isOverBudget ? (
+        <p style={{color: 'red'}}>${(props.totalSetBudget - props.totalSetIncome).toLocaleString('en-US', {maximumFractionDigits: 2, minimumFractionDigits: 2})} over budget</p>
+      ) : (
+        <>
+        {props.totalSetIncome === props.totalSetBudget ? (
+          <p style={{color: 'green'}}>You've got a Better Budget!</p>
         ) : (
-          <Button
-            type="submit"
-            value="Add Category"
-            kind="btn--primary--green"
-            disabled={false}
-            onClick={showForm}
-          ></Button>
+          <p>${(props.totalSetIncome - props.totalSetBudget).toLocaleString('en-US', {maximumFractionDigits: 2, minimumFractionDigits: 2})} left to budget</p>
         )}
-      </>
+        </>
+      )}
+          {props.categories.map((category, index) => {
+            return (
+              <Card key={index}>
+                <DashboardCategory
+                  transactions={props.transactions}
+                  category={category}
+                  subcategories={category.subcategories}
+                  year={props.year}
+                  month={props.month}
+                  day={props.day}
+                  onDeleteCategory={deleteCategory}
+                  onDeleteSubcategory={props.onDeleteSubcategory}
+                  onAddSubcategory={onAddSubcategory}
+                  onUpdateBudget={onUpdateBudget}
+                  deleteTransactions={deleteTransactions}
+                />
+              </Card>
+            );
+          })}
+          {isFormShown ? (
+            <form
+              className={style["dashboard__categories__form"]}
+              method="POST"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <input
+                type="text"
+                placeholder="Category"
+                {...register("category")}
+              />
+              <button type="submit">Add</button>
+              <button type="button" onClick={hideForm}>
+                Cancel
+              </button>
+            </form>
+          ) : (
+            <Button
+              type="submit"
+              value="Add Category"
+              kind="btn--primary--green"
+              disabled={false}
+              onClick={showForm}
+            ></Button>
+          )}
+        </>
       )}
     </div>
   );
