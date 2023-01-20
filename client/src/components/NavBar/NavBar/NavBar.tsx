fca231391a224507ccbd3b5ace229e6b1d38a9dc
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-19 19:29:54 -0500

    Add wrapper div

--- client/src/components/NavBar/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar/NavBar.tsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import MobileNav from "../MobileNav/MobileNav";
 import Nav from "../Nav/Nav";
 import style from "./NavBar.module.css";
-import {FaMoneyBillWave} from 'react-icons/fa';
+import { FaMoneyBillWave } from "react-icons/fa";
 export default function NavBar() {
   return (
     <div className={style["nav-container"]}>
-      <div className={style['nav-container__logo-container']}>
-        <Link to={"/"} className={style["logo"]}>
-          <FaMoneyBillWave color="#3D9970" size='40px'/>
-          <span>Better Budget</span>
-        </Link>
+      <div className={style['nav-content']}>
+        <div className={style["nav-container__logo-container"]}>
+          <Link to={"/"} className={style["logo"]}>
+            <FaMoneyBillWave color="#3D9970" size="40px" />
+            <span>Better Budget</span>
+          </Link>
+        </div>
+        <MobileNav />
+        <Nav />
       </div>
-      <MobileNav />
-      <Nav />
     </div>
   );
 }
