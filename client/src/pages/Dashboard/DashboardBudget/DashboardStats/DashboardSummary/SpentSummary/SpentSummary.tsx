97787665864f61583baf6b55ce8483a59da5984b
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-19 19:33:46 -0500

    Add component code

--- client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardSummary/SpentSummary/SpentSummary.tsx
+++ client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardSummary/SpentSummary/SpentSummary.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { CategoryInterface } from "../../../../../../Interfaces/CategoryInterface";
 import { TransactionInterface } from "../../../../../../Interfaces/TransactionInterface";
+import style from './SpentSummary.module.css';
 
 const SpentSummary = (props: {
   categories: CategoryInterface[];
@@ -8,29 +9,69 @@ const SpentSummary = (props: {
 }) => {
   const [categories, setCategories] = useState<CategoryInterface[]>([]);
   const [transactions, setTransactions] = useState<TransactionInterface[]>([]);
+  const [totalSpent, setTotalSpent] = useState<number>(0);
+
 
   useEffect(() => {
     setCategories(props.categories);
     setTransactions(props.transactions);
+    let totalSpent = 0;
+    props.transactions.forEach((trx) => {
+      if (trx.category !== "Income") {
+        totalSpent += trx.value;
+      }
+    });
+    setTotalSpent(totalSpent);
   }, [props.categories, props.transactions]);
+
+  useEffect(() => {
+    let totalSpent = 0;
+    props.transactions.forEach((trx) => {
+      if (trx.category !== "Income") {
+        totalSpent += trx.value;
+      }
+    });
+    setTotalSpent(totalSpent);
+  }, []);
 
   return (
     <div>
       {transactions.length > 0 ? (
         <ul>
-           {categories.map((category) => {
+           {categories.map((category, index) => {
             if(category.title !== 'Income') {
+              let currSpent = transactions.reduce((acc, curr) => {
+                if(curr.category === category.title) {
+                  return acc += curr.value;
+                }
+                return acc;
+               }, 0)
               return (
-                <li>
-                  {category.title} ${
-                   transactions.reduce((acc, curr) => {
-                    if(curr.category === category.title) {
-                      return acc += curr.value;
-                    }
-                    return acc;
-                   }, 0).toLocaleString('en-US', {maximumFractionDigits: 2, minimumFractionDigits: 2})
-                  }
-                </li>
+                <li key={index} className={style['spent-item']}>
+                <span className={style["spent-title"]}>{category.title}</span>
+                <span className={style["spent-amount"]}>
+                  <span>
+                    $
+                    {currSpent.toLocaleString("en-US", {
+                      maximumFractionDigits: 2,
+                      minimumFractionDigits: 2,
+                    })}
+                  </span>
+                  <span className={style["spent-percentage"]}>
+                  (
+                    {totalSpent > 0
+                      ? ((currSpent / totalSpent) * 100).toLocaleString(
+                          "en-US",
+                          {
+                            maximumFractionDigits: 2,
+                            minimumFractionDigits: 2,
+                          }
+                        )
+                      : 0}
+                    %)
+                  </span>
+                </span>
+              </li>
               );
             }
             return <></>
