52b1649ba770c37289e5fb407af819bf1052073f
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-19 19:33:15 -0500

    Add component code

--- client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardSummary/BudgetSummary/BudgetSummary.tsx
+++ client/src/pages/Dashboard/DashboardBudget/DashboardStats/DashboardSummary/BudgetSummary/BudgetSummary.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { CategoryInterface } from "../../../../../../Interfaces/CategoryInterface";
 import { TransactionInterface } from "../../../../../../Interfaces/TransactionInterface";
+import style from "./BudgetSummary.module.css";
 
 const BudgetSummary = (props: {
   categories: CategoryInterface[];
@@ -8,25 +9,71 @@ const BudgetSummary = (props: {
 }) => {
   const [categories, setCategories] = useState<CategoryInterface[]>([]);
   const [transactions, setTransactions] = useState<TransactionInterface[]>([]);
+  const [totalBudget, setTotalBudget] = useState<number>(0);
 
   useEffect(() => {
     setCategories(props.categories);
     setTransactions(props.transactions);
+
+    let totalBudget = 0;
+    props.categories.forEach((category) => {
+      if (category.title !== "Income") {
+        totalBudget += category.subcategories.reduce((acc, prev) => {
+          return (acc += prev.budget);
+        }, 0);
+      }
+    });
+    setTotalBudget(totalBudget);
   }, [props.categories, props.transactions]);
+
+  useEffect(() => {
+    let totalBudget = 0;
+    props.categories.forEach((category) => {
+      if (category.title !== "Income") {
+        totalBudget += category.subcategories.reduce((acc, prev) => {
+          return (acc += prev.budget);
+        }, 0);
+      }
+      setTotalBudget(totalBudget);
+    });
+  }, []);
 
   return (
     <div>
       {categories.length > 0 ? (
         <ul>
-          {categories.map((category) => {
-            if(category.title !== 'Income') {
+          {categories.map((category, index) => {
+            let currBudget = category.subcategories.reduce((acc, curr) => {
+              return (acc += curr.budget);
+            }, 0);
+            if (category.title !== "Income") {
               return (
-                <li>
-                  {category.title} ${
-                   category.subcategories.reduce((acc, curr) => {
-                    return acc += curr.budget;
-                   }, 0).toLocaleString('en-US', {maximumFractionDigits: 2, minimumFractionDigits: 2})
-                  }
+                <li className={style["budget-item"]} key={index}>
+                  <span className={style["budget-title"]}>
+                    {category.title}
+                  </span>
+                  <div className={style["budget-amount"]}>
+                    <span>
+                      $
+                      {currBudget.toLocaleString("en-US", {
+                        maximumFractionDigits: 2,
+                        minimumFractionDigits: 2,
+                      })}
+                    </span>
+                    <span className={style['budget-percentage']}>
+                      (
+                      {totalBudget > 0
+                        ? ((currBudget / totalBudget) * 100).toLocaleString(
+                            "en-US",
+                            {
+                              maximumFractionDigits: 2,
+                              minimumFractionDigits: 2,
+                            }
+                          )
+                        : 0}
+                      %)
+                    </span>
+                  </div>
                 </li>
               );
             }
