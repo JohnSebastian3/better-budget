f2f5cf52b0098adacc3b77b019d5deb5e74b991d
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-21 21:11:52 -0500

    Update form and add error messages

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,59 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect, FormEvent } from "react";
 import axios from "axios";
 import Footer from "../../components/Footer/Footer";
-export default function Login() {
+import Card from "../../components/UI/Card/Card";
+import style from "./Login.module.css";
+import Button from "../../components/UI/Button/Button";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLParagraphElement>(null);
+
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [errMsg, setErrMsg] = useState<string>("");
+  const [success, setSuccess] = useState<boolean>(false);
 
-  const login = () => {
-    axios
+  useEffect(() => {
+    if(emailRef.current) {
+      emailRef.current.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [email, password])
+
+  const login = async (e: FormEvent) => {
+    e.preventDefault();
+
+
+    try {
+      const res = await axios
       .post(
-        "https://better-budget-production.up.railway.app/login",
+        "http://localhost:4000/login",
         {
-          email,
+          email: email.toLowerCase(),
           password,
         },
         {
           withCredentials: true,
         }
       )
-      .then((res) => {
-        if (res.data === "OK") {
-          window.location.href = "/dashboard";
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
+      setSuccess(true);
+      setEmail('');
+      setPassword('');
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
+      window.location.href = "/dashboard";
+    } catch(err: any) {
+      if (!err?.response) {
+        setErrMsg('No Server Response');
+      } else if (err.response?.status === 400) {
+        setErrMsg('Missing Email or Password');
+      } else if (err.response?.status === 401) {
+        setErrMsg('Incorrect Email or Password');
+      } else {
+        setErrMsg('Log In Failed');
+      }
+     }
+    
   };
 
   return (
     <>
-      <div>
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="email"
-          onChange={handleEmailChange}
-          value={email}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={handlePasswordChange}
-          value={password}
-        />
-        <button onClick={login}>Login</button>
+      <div className={style["login"]}>
+        <Card>
+          <div className={style["login-container"]}>
+            <form onSubmit={(e) => login(e)}>
+              <div className={style["login-header"]}>
+                <h1>Log In</h1>
+                <p
+                    ref={errRef}
+                    className={errMsg ? style['errmsg'] : style['offscreen']}
+                    aria-live="assertive"
+                  >
+                    {errMsg}
+                  </p>
+              </div>
+              <div className={style["login-inputs"]}>
+                <div className={style["email-wrapper"]}>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    id="email"
+                    ref={emailRef}
+                    autoComplete="off"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                </div>
+                <div className={style["password-wrapper"]}>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                  />
+                </div>
+              </div>
+              <div className={style["form-button"]}>
+                    <Button
+                      type="submit"
+                      kind="btn--primary--green"
+                      modifier="btn--small"
+                      value="Log In"
+                      disabled={
+                        false
+                      }
+                    />
+                  </div>
+            </form>
+          </div>
+        </Card>
+        <span className={style["register-link"]}>
+          Don't have an account?
+          <Link to={"/register"} className={style["link"]}>
+            Create an account!
+          </Link>
+        </span>
       </div>
       <Footer></Footer>
     </>
   );
-}
+};
+
+export default Login;
