957dd35a2d9a3d4db00dc75796b5ddca71d217bf
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-01-22 18:35:04 -0500

    Update cors origin

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -7,8 +7,11 @@ router.post('/addTransaction', dashboardController.addTransaction);
 router.post('/addCategory', dashboardController.addCategory);
 router.post('/addSubcategory/:category', dashboardController.addSubcategory);
 router.post('/createBudget', dashboardController.createBudget);
+router.put('/updateCategory/:category/:year/:month', dashboardController.updateCategoryTitle);
 router.put('/updateSubcategory/:category/:subcategory/:year/:month', dashboardController.updateSubcategoryTitle);
 router.put('/setSubcategoryBudget/:category/:subcategory/:year/:month', dashboardController.setSubcategoryBudget);
+router.put('/updateTransactionTitle/:id', dashboardController.updateTransactionTitle);
+router.put('/updateTransactionValue/:id', dashboardController.updateTransactionValue);
 router.delete('/deleteSubcategory/:category/:subcategory/:month/:year/:day', dashboardController.deleteSubcategory);
 router.delete('/deleteCategory/:category/:month/:year/:day', dashboardController.deleteCategory);
 router.delete('/deleteTransaction/:id', dashboardController.deleteTransaction)
